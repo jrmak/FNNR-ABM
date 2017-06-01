85aeb5563d8dafb87658b9a38488e67214a568c4
jrmak <jmak@sdsu.edu>
2017-06-01 13:39:23 -0700

    Delete __main__.py

--- __main__.py
+++
@@ -1,9 +0,0 @@
-from agents import *
-from model import *
-from excel_import import *
-
-if __name__ == "__main__":
-    model = ABM(50, 10, 10) #(number of agents, grid width, grid height)
-    N = 20 #number of steps
-    for i in range(N):
-        model.step()
