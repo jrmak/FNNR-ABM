7dc27ff3e30cc436ecec6a7ec758637991dfda10
Judy <jmak@sdsu.edu>
2017-06-07 13:45:29 -0700

    Add files via upload

---
+++ agents.py
@@ -0,0 +1,109 @@
+# !/usr/bin/python
+
+"""
+This document defines agents and its attributes.
+It also defines what occurs to the agents at each 'step' of the ABM.
+"""
+
+from mesa import Agent  # Agent superclass from mesa
+from random import random
+from excel_import import *
+
+class HouseholdAgent(Agent):  # child class of Mesa's generic Agent class
+    """Sets household data and head-of-house info"""
+    def __init__(self, hh_id, model, pos, admin_village = 1, nat_village = 1, land_area = 100,
+                 charcoal = 10, GTGP_dry = 50, GTGP_rice = 50, total_dry = 50, total_rice = 50,
+                 NCFP = 1, num_mig = 1, income = 100, mig_prob = 0.5, num_labor = 20,
+                 min_req_labor = 10, comp_sign = 0.1, GTGP_coef = 0, GTGP_part = 0):
+
+        super().__init__(hh_id, model)
+        self.pos = pos  # resident location
+        self.admin_village = admin_village
+        self.nat_village = nat_village
+        self.charcoal = charcoal # consumption
+        self.land_area = land_area  # total land area for household
+        self.GTGP_dry = GTGP_dry  # area
+        self.GTGP_rice = GTGP_rice # area
+        self.total_dry = total_dry  # area
+        self.total_rice = total_rice  # area
+        self.NCFP = NCFP  # another PES program
+        self.num_mig = num_mig  # how many migrants the hh has
+
+        self.GTGP_part = GTGP_part  # binary (GTGP status of household)
+        self.income = income  # yearly household income
+        self.mig_prob = mig_prob  # migration probability, preset 0.5
+        self.num_labor = num_labor  # people in hh who can work, preset to 15-65
+        self.min_req_labor = min_req_labor  # preset
+        self.comp_sign = comp_sign  # influence of GTGP income on migration decisions
+        self.GTGP_coef = GTGP_coef  # randomly generated
+        # more attributes will be added later on
+
+    def step(self):
+        """Step behavior for household agents; see pseudo-code document"""
+        self.admin_village = 1
+
+# class CommunityAgent(Agent):
+    # will set attributes later on
+
+class IndividualAgent(HouseholdAgent):
+    """Sets Individual agents; superclass is HouseholdAgent"""
+    def __init__(self, individual_id, model):
+        super().__init__(self, individual_id, model, age = 20, gender = 1, education = 1)
+        self.age = age
+        self.gender = gender
+        self.education = education
+
+class LandParcelAgent(HouseholdAgent):
+    """Sets land parcel agents; superclass is HouseholdAgent"""
+    def __init__(self, land_id, model, pos, GTGP_part_flag = 0, area = 1, latitude = 0,
+                    longitude = 0, plant_type = 1):
+
+        super().__init__(self, land_id, model)
+        self.pos = pos
+        self.GTGP_part_flag = GTGP_part_flag  # binary (GTGP status of land parcel)
+        self.area = area
+        self.latitude = latitude
+        self.longitude = longitude
+        self.plant_type = plant_type
+
+    def gtgp_change(self):
+        """See pseudo-code document: predicts GTGP participation per household"""
+        for hh in hh_id_list:  # for each household,
+            self.num_mig = convert_num_mig(return_values(hh, 'num_mig'))  # sets num_mig in hh
+            agelist = return_values(hh, 'age')  # find the ages of people in hh
+            if agelist[0] is not None:
+                self.num_labor = len(agelist)  # defines number of laborers as people aged 15 < x < 65
+            for age in agelist:  # for each person,
+                try:
+                    if 15 < float(age) < 65:  # if at least one person is 15-65 years old,
+                        # if return_values(i,'GTGP_area') != 'None' and return_values(i, 'GTGP_area') != '[]':
+                        self.GTGP_part = 1  # enroll them
+                        break  # avoid redundant flagging
+                except:
+                    pass  # covers situations in which age is 'NoneType'
+            if self.GTGP_part_flag == 0:  # test
+                self.GTGP_coef = random.uniform(0, 0.51)  # random coefficient
+                self.GTGP_comp = 15  # calculated later
+            if (self.GTGP_coef * self.GTGP_part) > self.mig_prob and (self.GTGP_comp / self.income) > self.comp_sign:
+                if self.num_labor > 0:
+                    self.num_labor -= 1
+                    self.num_mig += 1  # migration occurs
+            if self.num_labor < self.min_req_labor:
+                self.GTGP_part_flag = 1  # sets flag for enrollment of more land
+
+    def gtgp_test(self):
+        """Basic formula for testing web browser simulation; each step, 5% of agents change flags"""
+        chance = random.random()
+        if chance > 0.95:
+            self.GTGP_part_flag = 1
+
+    def step(self):
+        """Step behavior for LandParcelAgent"""
+        self.gtgp_change()
+
+class PESAgent(Agent):
+    """Sets PES policy agents"""
+    def __init__(self, policy_id, model, GTGP_comp):
+        super().__init__(policy_id, model)
+        self.GTGP_comp = GTGP_comp
+        # more attributes will be added later on
---
+++ model.py
@@ -0,0 +1,106 @@
+# !/usr/bin/python
+
+"""
+This document runs the main model, placing agents into the ABM.
+"""
+
+from mesa import Model
+from mesa.time import RandomActivation
+from mesa.space import ContinuousSpace
+from agents import *
+from excel_import import *
+
+
+class ABM(Model):
+    """Handles agent creation, placement, and value changes"""
+    def __init__(self, n_agents, width, height, GTGP_land = 0, GTGP_latitude = 0, GTGP_longitude = 0,
+                 num_mig = 0, mig_prob = 0.5, min_req_labor = 0, num_labor = 0, GTGP_part = 0,
+                 GTGP_coef = 0, GTGP_part_flag = 0, area = 1, admin_village = 1):
+        # default values set for now, will define when model runs agents
+
+        self.num_agents = n_agents
+        self.GTGP_land = GTGP_land
+        self.GTGP_latitude = GTGP_latitude
+        self.GTGP_longitude = GTGP_longitude
+        self.num_mig = num_mig
+        self.mig_prob = mig_prob
+        self.min_req_labor = min_req_labor
+        self.num_labor = num_labor
+        self.GTGP_part = GTGP_part
+        self.GTGP_coef = GTGP_coef
+        self.GTGP_part_flag = GTGP_part_flag
+        self.area = area
+        self.admin_village = admin_village
+
+        self.space = ContinuousSpace(width, height, True, grid_width = 10, grid_height = 10)
+        # class space.ContinuousSpace(x_max, y_max, torus, x_min=0, y_min=0, grid_width=100, grid_height=100)
+        # methods: get_distance, get_neighbors, move_agent, out_of_bounds, place_agent
+        self.schedule = RandomActivation(self)
+        self.make_hh_agents()
+        self.make_land_agents()
+        self.running = True
+
+    def determine_pos(self, hh_id, latitude, longitude):
+        """Determine position of agent on map"""
+        try:
+            x = convert_fraction_lat(
+                convert_lat_long(
+                    str(return_values(hh_id, latitude))
+                )
+            ) * self.space.x_max
+
+            y = convert_fraction_long(
+                convert_lat_long(
+                    str(return_values(hh_id, longitude))
+                )
+            ) * self.space.y_max
+            pos = (x, y)
+            return pos
+        except:
+            pass
+
+    # Create agents
+    def make_hh_agents(self):
+        """Create the household agents"""
+        for hh_id in hh_id_list:  # from excel_import
+            pos = self.determine_pos(hh_id, 'house_latitude', 'house_longitude')
+            try:
+                hh = HouseholdAgent(hh_id, self, pos, self.admin_village, self.GTGP_part, self.GTGP_land,
+                                    self.GTGP_coef, self.mig_prob, self.num_mig, self.min_req_labor,
+                                    self.num_labor)
+                hh.admin_village = 1
+                self.space.place_agent(hh, pos)  # admin_village placeholder
+                self.schedule.add(hh)
+            except:
+                pass
+
+    def make_land_agents(self):
+        """Create the land agents on the map"""
+        # add non-GTGP land parcels
+        for hh_id in hh_id_list:  # from excel_import
+            pos = self.determine_pos(hh_id, 'non_GTGP_latitude', 'non_GTGP_longitude')
+            try:
+                lp = LandParcelAgent(hh_id, self, pos, self.area, self.GTGP_part_flag)
+                lp.GTGP_part_flag = 0
+                self.space.place_agent(lp, pos)
+                self.schedule.add(lp)
+            except:
+                pass
+        # add GTGP land parcels
+        for hh_id in hh_id_list:  # from excel_import
+            pos = self.determine_pos(hh_id, 'GTGP_latitude', 'GTGP_longitude')
+            try:
+                lp = LandParcelAgent(hh_id, self, pos, self.area, self.GTGP_part_flag)
+                lp.GTGP_part_flag = 1
+                self.space.place_agent(lp, pos)
+                self.schedule.add(lp)
+            except:
+                # hh_id_list.remove(hh_id)
+                # print(hh_id_list)
+                pass
+
+    def step(self):
+        """Advance the model by one step"""
+        self.schedule.step()
+        #for i in range(10):
+        #    self.schedule.step()  # run 10 steps at once
